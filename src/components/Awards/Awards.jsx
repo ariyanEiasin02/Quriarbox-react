3a5dc5eec3d83b29a201f936ed250a5d54e3bd85
Md Eiasin <ariyaneiasin02@gmail.com>
2024-05-27 20:15:56 +0600

    first commit

--- src/components/Awards/Awards.jsx
+++ src/components/Awards/Awards.jsx
@@ -1,48 +1,50 @@
 import React from 'react'
-import AwardsRap from './AwardsRap'
-const AwardsItems =[
-    {  
-      iconUrl : "/src/assets/LINE---COLOR.png", 
-      number : "26+",
-      awardHeter : "Awards won"      
-    },
-    {  
-      iconUrl : "/src/assets/IconSpace_Map_24Px.png", 
-      number : "65+",
-      awardHeter : "States covered"      
-    },
-    {  
-      iconUrl : "/src/assets/iconspace_Man_25px.png", 
-      number : "689k+",
-      awardHeter : "Happy clients"      
-    },
-    {  
-      iconUrl : "/src/assets/iconspace_Cement Truck_25px.png", 
-      number : "130M+",
-      awardHeter : "Goods delivered"      
-    },
-    {  
-      iconUrl : "/src/assets/iconspace_Business Bag_25px.png", 
-      number : "130M+",
-      awardHeter : "Business hours"      
-    },
-    {  
-      iconUrl : "/src/assets/IconSpace_Map_24Px.png", 
-      number : "65M+",
-      awardHeter : "Business hours"      
-    }
-]
-const Award = AwardsItems.map((items) => <AwardsRap awardItems={items}></AwardsRap>)
+import imgOne from '../../assets/LINE---COLOR.png'
+import imgTwo from '../../assets/IconSpace_Map_24Px.png'
+import imgThree from '../../assets/iconspace_Man_25px.png'
+import imgFour from '../../assets/iconspace_Cement Truck_25px.png'
+import imgFive from '../../assets/iconspace_Business Bag_25px.png'
+import imgSix from '../../assets/IconSpace_Map_24Px.png'
+ 
 const Awards = () => {
   return (
     <div>
-        <section id='awards' className='md:py-16 py-12'>
-            <div className="max-w-container mx-auto">
-                <div className="flex flex-wrap justify-between">
-                    {Award}
-                </div>
+      <section id='awards' className='md:py-16 py-12'>
+        <div className="max-w-container mx-auto">
+          <div className="flex flex-wrap justify-between">
+            <div className='items-center md:mt-0 mt-6 w-3/6 md:w-[16.65%]'>
+              <img className="mx-auto h-14" src={imgOne} alt="ami" />
+              <p className='text-4xl text-center text-pramary font-bold font-raleway py-2'>26+</p>
+              <h3 className='font-raleway text-center font-normal text-xl'>Awards won</h3>
             </div>
-        </section>
+            <div className='items-center md:mt-0 mt-6 w-3/6 md:w-[16.65%]'>
+              <img className="mx-auto h-14" src={imgTwo} alt="ami" />
+              <p className='text-4xl text-center text-pramary font-bold font-raleway py-2'>65+</p>
+              <h3 className='font-raleway text-center font-normal text-xl'>States covered</h3>
+            </div>
+            <div className='items-center md:mt-0 mt-6 w-3/6 md:w-[16.65%]'>
+              <img className="mx-auto h-14" src={imgThree} alt="ami" />
+              <p className='text-4xl text-center text-pramary font-bold font-raleway py-2'>689k+</p>
+              <h3 className='font-raleway text-center font-normal text-xl'>Happy clients</h3>
+            </div>
+            <div className='items-center md:mt-0 mt-6 w-3/6 md:w-[16.65%]'>
+              <img className="mx-auto h-14" src={imgFour} alt="ami" />
+              <p className='text-4xl text-center text-pramary font-bold font-raleway py-2'>130M+</p>
+              <h3 className='font-raleway text-center font-normal text-xl'>Goods delivered</h3>
+            </div>
+            <div className='items-center md:mt-0 mt-6 w-3/6 md:w-[16.65%]'>
+              <img className="mx-auto h-14" src={imgFive} alt="ami" />
+              <p className='text-4xl text-center text-pramary font-bold font-raleway py-2'>130M+</p>
+              <h3 className='font-raleway text-center font-normal text-xl'>Business hours</h3>
+            </div>
+            <div className='items-center md:mt-0 mt-6 w-3/6 md:w-[16.65%]'>
+              <img className="mx-auto h-14" src={imgSix} alt="ami" />
+              <p className='text-4xl text-center text-pramary font-bold font-raleway py-2'>265K+</p>
+              <h3 className='font-raleway text-center font-normal text-xl'>Business hours</h3>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,58 +1,74 @@
 
 import React from 'react'
-import SerRap from './SerRap'
-const cardItems = [
-    {   
-        imageUrl : "/src/assets/iCON.svg",
-        header : "Business Services",
-        paragharap : "We give you complete reliable delivery for your company.  We will take full responsibility of the deliveries.",
-        navLink :{
-            navLinkOne : "Corporate goods",
-            navLinkTwo : "Shipment",
-            navLinkThree : "Corporate goods",
-        },
-        btn : "Learn more"
-    },
-    {   
-        imageUrl : "/src/assets/Icon2.svg",
-        header : "Statewide Services",
-        paragharap : "Offering home delivery around the city, where your products will be at your doorstep within 48-72 hours.",
-        navLink :{
-            navLinkOne : "Personal items",
-            navLinkTwo : "Percels",
-            navLinkThree : "Documents",
-        },
-        btn : "Learn more"
-    },
-    {   
-        imageUrl : "/src/assets/Icon3.svg",
-        header : "Personal Services",
-        paragharap : "You can trust us to safely deliver your most sensitive documents to the specific area in a short time.",
-        navLink :{
-            navLinkOne : "Gifts",
-            navLinkTwo : "Package",
-            navLinkThree : "Documents",
-        },
-        btn : "Learn more"
-    }
-]
-const ban = cardItems.map((items) => <SerRap userItems={items}></SerRap>)
+import serImgOne from '../../assets/iCON.svg'
+import serImgTwo from '../../assets/Icon2.svg'
+import serImgThree from '../../assets/Icon3.svg'
 const Service = () => {
-  return (
-    <>
-      <section id='service' className='py-14'>
-        <div className="max-w-container mx-auto">
-            <div className="text-center">
-                <h2 className='font-raleway text-bold text-3xl text-pramary'>SERVICES</h2>
-                <h3 className='font-raleway font-extrabold text-3xl text-service'>Our services for you</h3>
-            </div>
-            <div className="md:flex md:justify-between">
-                {ban}
-            </div>
-        </div>
-      </section>
-    </>
-  )
+    return (
+        <>
+            <section id='service' className='py-14'>
+                <div className="max-w-container mx-auto">
+                    <div className="text-center">
+                        <h2 className='font-raleway text-bold text-3xl text-pramary'>SERVICES</h2>
+                        <h3 className='font-raleway font-extrabold text-3xl text-service'>Our services for you</h3>
+                    </div>
+                    <div className="md:flex md:justify-between">
+                        <div className="md:w-[30%] w-[90%] md:mx-0 mx-auto bg-white py-10 px-10 rounded-lg mt-14">
+                            <div className="">
+                                <div className="text-center">
+                                    <img className="mx-auto" src={serImgOne} alt="Description of the image" />
+                                    <h2 className="text-2xl font-raleway font-bold my-2">Business Services</h2>
+                                </div>
+                                <p className='text-sm font-raleway font-light'>We give you complete reliable delivery for your company.  We will take full responsibility of the deliveries.</p>
+                                <ul className='list-disc py-2'>
+                                    <li className='text-base font-raleway font-light text-black mt-2'>Corporate goods</li>
+                                    <li className='text-base font-raleway font-light text-black mt-2'>Shipment</li>
+                                    <li className='text-base font-raleway font-light text-black mt-2'>Corporate goods</li>
+                                </ul>
+                                <div className="hover:bg-pramary border-pramary border-2 border-solid rounded text-center mt-6">
+                                    <a className='text-base py-2 px-4 w-full text-pramary font-raleway hover:text-white block font-medium ' href="#">Learn more</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="md:w-[30%] w-[90%] md:mx-0 mx-auto bg-white py-10 px-10 rounded-lg mt-14">
+                            <div className="">
+                                <div className="text-center">
+                                    <img className="mx-auto" src={serImgTwo} alt="Description of the image" />
+                                    <h2 className="text-2xl font-raleway font-bold my-2">Statewide Services</h2>
+                                </div>
+                                <p className='text-sm font-raleway font-light'>Offering home delivery around the city, where your products will be at your doorstep within 48-72 hours.</p>
+                                <ul className='list-disc py-2'>
+                                    <li className='text-base font-raleway font-light text-black mt-2'>Personal items</li>
+                                    <li className='text-base font-raleway font-light text-black mt-2'>Percels</li>
+                                    <li className='text-base font-raleway font-light text-black mt-2'>Documents</li>
+                                </ul>
+                                <div className="hover:bg-pramary border-pramary border-2 border-solid rounded text-center mt-6">
+                                    <a className='text-base py-2 px-4 w-full text-pramary font-raleway hover:text-white block font-medium ' href="#">Learn more</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="md:w-[30%] w-[90%] md:mx-0 mx-auto bg-white py-10 px-10 rounded-lg mt-14">
+                            <div className="">
+                                <div className="text-center">
+                                    <img className="mx-auto" src={serImgThree} alt="Description of the image" />
+                                    <h2 className="text-2xl font-raleway font-bold my-2">Personal Services</h2>
+                                </div>
+                                <p className='text-sm font-raleway font-light'>You can trust us to safely deliver your most sensitive documents to the specific area in a short time.</p>
+                                <ul className='list-disc py-2'>
+                                    <li className='text-base font-raleway font-light text-black mt-2'>Gifts</li>
+                                    <li className='text-base font-raleway font-light text-black mt-2'>Package</li>
+                                    <li className='text-base font-raleway font-light text-black mt-2'>Documents</li>
+                                </ul>
+                                <div className="hover:bg-pramary border-pramary border-2 border-solid rounded text-center mt-6">
+                                    <a className='text-base py-2 px-4 w-full text-pramary font-raleway hover:text-white block font-medium ' href="#">Learn more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default Service
